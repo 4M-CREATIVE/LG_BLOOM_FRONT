68634641bf97335ac3876c01d6f1a3539e568ac3
DESKTOP-QC78LBU\4M-FE <gasina85@gmail.com>
2025-04-07 10:14:36 +0900

    [Bloom] 헤더 메뉴 스크립트 수정

--- src/main/resource/static/js/user/common.js
+++ src/main/resource/static/js/user/common.js
@@ -1,37 +1,38 @@
-document.addEventListener('DOMContentLoaded', function () {
-  browserDetector();       // 브라우저 감지
-  headerMenuToggle();      // 헤더 메뉴 토글
-});
+var commonEvent = {
+  init: function () {
+    this.headerEvent(); //헤더 이벤트
+  },
+  headerEvent: () => {
+    // 브라우저감지
+    if (navigator.userAgent.includes('Edg')) {
+      document.body.classList.add('edge');
+      alert('edge');
+    }
+  
+    if (navigator.userAgent.includes('Whale')) {
+      document.body.classList.add('whale');
+      alert('whale');
+    }
 
-// 브라우저 감지
-function browserDetector() {
-  if (navigator.userAgent.includes('Edg')) {
-    document.body.classList.add('edge');
-    alert('edge');
-  }
-
-  if (navigator.userAgent.includes('Whale')) {
-    document.body.classList.add('whale');
-    alert('whale');
-  }
-}
-
-// 헤더 메뉴 토글
-function headerMenuToggle() {
-  const gnb = document.querySelector('.gnb');
-  const menuBtn = document.querySelector('.menu-open-btn');
-  const closeBtn = document.querySelector('.gnb .close-btn');
-
-  if (!gnb || !menuBtn || !closeBtn) {
-    console.warn('필요한 요소를 찾지 못했습니다.');
-    return;
-  }
-
-  menuBtn.addEventListener('click', function () {
-    gnb.classList.add('open');
-  });
-
-  closeBtn.addEventListener('click', function () {
-    gnb.classList.remove('open');
-  });
+    // 헤더 메뉴 토글
+    const gnb = document.querySelector('.gnb');
+    const menuBtn = document.querySelector('.menu-open-btn');
+    const closeBtn = document.querySelector('.gnb .close-btn');
+  
+    if (!gnb || !menuBtn || !closeBtn) {
+      console.log(gnb);
+      console.log(menuBtn);
+      console.log(closeBtn);
+      console.warn('필요한 요소를 찾지 못했습니다.');
+      return;
+    }
+  
+    menuBtn.addEventListener('click', function () {
+      gnb.classList.add('open');
+    });
+  
+    closeBtn.addEventListener('click', function () {
+      gnb.classList.remove('open');
+    });
+  },
 }
